c039b5b9991e89feb75020d409b824cf5faffecf
Mario Salazar <mariosalazar.ms.10@gmail.com>
2025-08-11 14:18:50 -0500

    feat(factory): add new module for role users

--- src/factory/prisma/modules-using-prisma.module.ts
+++ src/factory/prisma/modules-using-prisma.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModuleUsingPrisma } from 'src/modules/authentication/infrastructure/modules/prisma.auth.module';
 import { PermissionModuleUsingPrisma } from 'src/modules/permission/infrastructure/modules/prisma.permission.module';
 import { RolePermissionModuleUsingPrisma } from 'src/modules/roles/infrastructure/modules/role-permission.prisma.module';
+import { RoleUserModuleUsingPrisma } from 'src/modules/roles/infrastructure/modules/role-user.prisma.module';
 import { UserTypeModuleUsingPrisma } from 'src/modules/user-type/infrastructure/module/user-type.prisma.module';
 import { UserModuleUsingPrisma } from 'src/modules/users/infrastructure/module/user.prisma.module';
 
@@ -12,6 +13,7 @@ import { UserModuleUsingPrisma } from 'src/modules/users/infrastructure/module/u
     AuthModuleUsingPrisma,
     PermissionModuleUsingPrisma,
     RolePermissionModuleUsingPrisma,
+    RoleUserModuleUsingPrisma,
   ],
   controllers: [],
   providers: [],
